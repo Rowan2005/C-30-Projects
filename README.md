f5e40fa247f00130fbc17eec85011e38a7fdabdf
Rowan2005 <58427428+Rowan2005@users.noreply.github.com>
2020-03-03 16:31:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C-30-Projects
-Vanishing Blocks C-30
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ football.js
@@ -0,0 +1,10 @@
+class football extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/football.png");
+  }
+
+  display() {
+    super.display();
+  }
+}
